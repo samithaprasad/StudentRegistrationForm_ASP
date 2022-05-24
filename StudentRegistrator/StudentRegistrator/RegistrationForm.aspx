4bd6daaa09fb4200ab39b2bab62d216d680f8128
Samitha Prasad <91029360+samithaprasad@users.noreply.github.com>
2022-05-25 04:39:28 +0530

    Add files via upload

---
+++ StudentRegistrator/StudentRegistrator/RegistrationForm.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegistrationForm.aspx.cs" Inherits="StudentRegistrator.RegistrationForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+    <title>Student Registration Form</title>
+    <link rel="stylesheet"  href="Css/StyleSheet.css" />
+</head>
+<body>
+    <header>
+        <div class="class-1">
+            <p>ABC Institute</p>
+    <h1>Student Registration Form</h1>
+             <p>2021 Advance Level</p>
+    
+    </div>
+    </header>
+    <main>
+    
+    <form id="form1" runat="server">
+    <section>
+        <table style="width: 100%;">
+            <caption class="c1">Fill the form</caption> 
+            <tr>
+                <td ><b>Name with initials:</b></td><td><input type="text" name="Name" size="50" maxlength="60" style="height: 30px" /></td>
+            </tr>
+            <tr>
+                <td ><b>Address:</b></td>
+                <td><textarea name="Address"  rows="4" style="width: 297px" ></textarea></td>
+
+            </tr>
+            <tr>
+                <td><b>Contact No.:</b></td><td><input type="text" name="Contact" size="25" maxlength="12" style="height: 30px" /></td>
+            </tr>
+            <tr>
+                <td ><b>Email Address.:</b></td><td><input type="text" name="Email" size="50" maxlength="150" style="height: 30px" /></td>
+            </tr>
+            <tr>
+                <td></td><td>
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" Font-Bold="True" Font-Size="18px" Height="45px" Width="132px" /></td>
+            </tr>
+        </table>
+        
+	
+		
+	
+    </section>
+   
+    </form>
+    </main>
+    <footer>
+        <p>copyright &copy; 2021,</p>
+    </footer>
+</body>
+    
+   
+</html>
---
+++ StudentRegistrator/StudentRegistrator/RegistrationForm.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace StudentRegistrator
+{
+    public partial class RegistrationForm : System.Web.UI.Page
+    {
+         
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name = Request.Form.Get("Name");
+            string address = Request.Form.Get("Address");
+            string contact = Request.Form.Get("Contact");
+            string email = Request.Form.Get("Email");
+            insertData(name,address,contact,email);
+        }
+        
+        void insertData(string N,string A,string C, string E)
+        {
+            MySqlConnection con = connection();
+            string quary = "insert into login.student (FullName,PostalAddress,ContactNumber,Email) values ('"+N+"','"+A+"','"+C+"','"+E+"');";
+            MySqlCommand cmd = new MySqlCommand(quary, con);
+            con.Open();
+            try
+            {   cmd.ExecuteNonQuery();
+                Response.Write("<script>alert('Registration Successfully')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                return;
+            }
+            con.Close();
+        }
+
+        MySqlConnection connection()
+        {
+           
+            MySqlConnection con = null;
+            string connString = "server =localhost; Uid=root; password = toor;  database =login;"; 
+            try
+            {
+                con = new MySqlConnection(connString);
+            }
+            catch (Exception e)
+            {
+                Response.Write("<script>alert('" + e.Message + "')</script>");
+            }
+
+            return con;
+        }
+    }
+    
+}
